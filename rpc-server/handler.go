5a40062219e4ad6e18a896ddea55932305fd4242
Sebastian Lee <sebastian_lee@u.nus.edu>
2023-06-02 22:26:37 +0800

    Modified Send RPC function to execute insert query to db

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
-	"math/rand"
-
+	"strconv"
+	"github.com/sebbycake/tiktok_assignment_2023/rpc-server/db"
 	"github.com/sebbycake/tiktok_assignment_2023/rpc-server/kitex_gen/rpc"
+	"math/rand"
 )
 
 // IMServiceImpl implements the last service interface defined in the IDL.
@@ -12,7 +13,22 @@ type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
 	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+
+	// Create a new database connection
+	database, err := db.NewDB("admin", "password", "localhost", "instant_messaging_app")
+	if err != nil {
+		resp.Code, resp.Msg = 500, "Error connecting to database"
+		return resp, err
+	}
+	defer database.Close()
+
+	// Execute SQL insert query
+	id, err := database.Create("INSERT INTO messages (chat, text, sender, send_time) VALUES (?, ?, ?, ?)", req.Message.Chat, req.Message.Text, req.Message.Sender, req.Message.SendTime)
+	if err != nil {
+		resp.Code, resp.Msg = 500, "Error executing INSERT query"
+		return resp, err
+	}
+	resp.Code, resp.Msg = 0, strconv.FormatInt(id, 10)
 	return resp, nil
 }
 
