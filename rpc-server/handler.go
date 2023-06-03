82cc7f48aae1dd3e51c1b577f5a0b40d10f06ee2
Sebastian Lee <sebastian_lee@u.nus.edu>
2023-06-03 13:23:43 +0800

    Fix database connection string with Docker

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -15,7 +15,7 @@ func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.Se
 	resp := rpc.NewSendResponse()
 
 	// Create a new database connection
-	database, err := db.NewDB("root", "root_password", "localhost:3306", "instant_messaging_app")
+	database, err := db.NewDB("sebastian", "sebastian", "host.docker.internal:3306", "instant_messaging_app")
 	if err != nil {
 		resp.Code, resp.Msg = 500, "Error connecting to database"
 		return resp, err
